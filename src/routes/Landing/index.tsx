e89d67d0e0056e796779fec7452d5ce126931782
Jubille <jubille@poplarhomes.com>
2023-07-25 17:00:18 +0800

    Setup routes and configs

---
+++ src/routes/Landing/index.tsx
@@ -0,0 +1,5 @@
+const Landing = () => {
+  return <div>This is the landing page!</div>
+}
+
+export default Landing
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const env = Object.assign(process.env, loadEnv(mode, process.cwd()))
+  const port = parseInt(env.VITE_PORT) || 3000
+
+  return {
+    server: {
+      port,
+    },
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        inject: {
+          data: env,
+        },
+      }),
+    ],
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    resolve: {
+      alias: {
+        '@app': resolve(__dirname, 'src'),
+      },
+    },
+  }
 })
