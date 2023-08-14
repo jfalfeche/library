55aaa46c83440ccd058fc1c4998f59446fc6ad27
Jubille <jfalfeche27@gmail.com>
2023-08-14 23:06:33 +0800

    Remove unnecessary div

--- src/routes/Landing/components/Loading.tsx
+++ src/routes/Landing/components/Loading.tsx
@@ -5,11 +5,9 @@ const Loading: FC = () => {
   const video = '../../../../public/building.mp4'
 
   return (
-    <div>
-      <video playsInline autoPlay muted loop poster="polina.jpg" id="bgvid">
-        <source src={video} type="video/mp4" />
-      </video>
-    </div>
+    <video playsInline autoPlay muted loop poster="polina.jpg" id="bgvid">
+      <source src={video} type="video/mp4" />
+    </video>
   )
 }
 
